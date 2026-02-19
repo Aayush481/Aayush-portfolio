6d6581c09f84be780b46434cc0ff5383f8a83999
Aayush481 <aayush6b12@gmail.com>
2026-02-19 20:23:34 +0530

    updating netlify function

--- netlify/functions/sendEmail.js
+++ netlify/functions/sendEmail.js
@@ -4,18 +4,16 @@ export async function handler(event) {
   try {
     const body = JSON.parse(event.body);
 
-
     const transporter = nodemailer.createTransport({
       service: "gmail",
       host: "smtp.gmail.com",
       port: 465,
       secure: true,
       auth: {
-        user: "aayush6b12@gmail.com",
-        pass: "easg matc wooy ecms",
+        user: process.env.EMAIL_USER,
+        pass: process.env.EMAIL_PASS,
       },
     });
-
 
     const mailOptions = {
       from: `"${body.name}" <${body.email}>`,
@@ -23,12 +21,12 @@ export async function handler(event) {
       to: "aayush6b12@gmail.com",
       subject: "New Contact Form Submission",
       text: `
-    Name: ${body.name}
-    Email: ${body.email}
-    Service: ${body.service}
-    Budget: ${body.budget}
-    Idea: ${body.idea}
-  `,
+        Name: ${body.name}
+        Email: ${body.email}
+        Service: ${body.service}
+        Budget: ${body.budget}
+        Idea: ${body.idea}
+      `,
     };
 
     await transporter.sendMail(mailOptions);
